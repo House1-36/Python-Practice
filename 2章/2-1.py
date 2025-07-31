016e572a21e639a4be5de0e5ed65ea1427c4368c
House1-36 <hsh.house0324@gmail.com>
2025-07-31 09:31:44 +0900

    2章

---
+++ 2章/2-1.py
@@ -0,0 +1,17 @@
+#数式の計算
+
+#組み込み型の説明
+
+#整数型 int 
+#浮動小数点型 float
+#文字列型 str
+#真偽型 bool
+
+#実際に使ってみる
+
+# その文字列などの型を調べる関数はtype()
+
+print(type(1))      #int型
+print(type(3.14))   #float型
+print(type("Hi"))   #str型
+print(type(False))  #bool型
