a443679e23b4cf04090f84da253a1e28255bf6af
House1-36 <hsh.house0324@gmail.com>
2025-07-31 09:18:12 +0900

    エスケープシーケンスの確認

---
+++ 1-3.py
@@ -0,0 +1,8 @@
+#ここでエスケープシーケンスの確認をします。
+
+print("Hello, World!")  # 通常の文字列出力
+print("Hello,\nWorld!")  # 改行を含む文字列出力
+print("Hello,\tWorld!")  # タブを含む文字列出力
+print("Hello, \"World\"!")  # ダブルクォートを含む文字
+print("Hello, 'World'!")  # シングルクォートを含む文字列出力
+print("Hello, \\World!")  # バックスラッシュを含む文字列出
